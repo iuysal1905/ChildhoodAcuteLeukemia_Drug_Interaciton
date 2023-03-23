1bb3fca886729375de09609849ad11dcee001f83
ilhan uysal <63537319+iuysal1905@users.noreply.github.com>
2023-03-23 15:43:54 +0300

    Update lukemia_ddi.py

--- lukemia_ddi.py
+++ lukemia_ddi.py
@@ -7,7 +7,7 @@ import pip
 pip.main(["install", "openpyxl"])
 
 # Read dataset 
-df_interact = pd.read_excel('lukemia_ddi.xlsx')
+df_interact = pd.read_excel('leukemia_ddi.xlsx')
 
 # Set header title
 st.title('Network Graph Visualization of Drug-Drug Interactions')
