caba883aa034bb75f7d5ff6777f7006f6f4dcb83
ilhan uysal <63537319+iuysal1905@users.noreply.github.com>
2023-06-09 13:27:56 +0300

    Update arayuz.py

--- arayuz.py
+++ arayuz.py
@@ -6,41 +6,63 @@ from pyvis.network import Network
 import pip
 pip.main(["install", "openpyxl"])
 
-# Read dataset 
-df_interact = pd.read_excel('data1.xlsx')
-
 # Set header title
-st.title('Network Graph Visualization of Drug-Drug Interactions')
+st.title('XAI ile İlaç Yeniden Kullanım Çalışmaları için Benzetim Ortamı')
 
 # Define list of selection options and sort alphabetically
-drug_list = ['Amiodarone','Nefazodone','Phenobarbital','Venlafaxine','Primidone','Phenytoin','Fluvoxamine','Curcumin','Ziprasidone','Carbamazepine',
-'Fosphenytoin','Pentobarbital','Verapamil','Clozapine','Diltiazem','Vemurafenib','Cyclosporine','Pitolisant','Stiripentol','Rifampicin']
-drug_list.sort()
+sec1 = ['IC50','Inhibition']
+sec1.sort()
+sec2=['Biyoaktivite','XAI','Model Performansı']
+sec3=['Biyoaktivite Sınıfı','LogP','MW','NumHAcceptors','NumHDonors','PIC50']
+sec4=['WaterFall','Bar','BeeSwarm','HeatMap','Important Features']
+sec5=['RMSE','Time Taken']
 
 # Implement multiselect dropdown menu for option selection (returns a list)
-selected_drugs = st.multiselect('Select drug(s) to visualize', drug_list)
+selected_1 = st.multiselect('Lütfen standart türlerden birini seçiniz', sec1)
 
 # Set info message on initial site load
-if len(selected_drugs) == 0:
-    st.text('Choose at least 1 drug to start')
+if len(selected_1) == 0:
+    st.text('Lütfen listeden birini seçiniz')
 
 # Create network graph when user selects >= 1 item
-else:
-    df_select = df_interact.loc[df_interact['drug1_name'].isin(selected_drugs) | \
-                                df_interact['drug2_name'].isin(selected_drugs)]
-    df_select = df_select.reset_index(drop=True)
+elif (selected_1=='IC50'):
 
-    # Create networkx graph object from pandas dataframe
-    G = nx.from_pandas_edgelist(df_select, 'drug1_name', 'drug2_name', 'weight')
-
-    # Initiate PyVis network object
-    drug_net = Network(
-                       height='400px',
-                       width='100%',
-                       bgcolor='#222222',
-                       font_color='white'
-                      )
-
+    selected_2 = st.multiselect('Lütfen değerlendirme türlerinden birini seçiniz', sec2)
+    if len(selected_2) == 0:
+        st.text('Lütfen listeden birini seçiniz')
+    elif (selected_2=='Biyoaktivite'):
+        selected_3 = st.multiselect('Lütfen biyoaktivite türlerinden birini seçiniz', sec3)
+        if (selected_3=='Biyoaktivite Sınıfı'):
+            print('ic50_bioactivitiy_class.png')
+        elif (selected_3=='LogP'):
+            print('ic50_logp.png')
+        elif (selected_3=='MW'):
+            print('ic50_mw.png')
+        elif (selected_3=='NumHAcceptors'):
+            print('ic50_numhacceptors')
+        elif (selected_3=='NumHDonors'):
+            print('ic50_numhdonors')
+        elif (selected_3=='PIC50'):
+            print('ic50_pic50_bioclass')
+    elif (selected_2=='XAI'):
+        selected_4 = st.multiselect('Lütfen grafik türlerinden birini seçiniz', sec4)
+        if (selected_4=='WaterFall'):
+            print('ic50_waterfall.png')
+        elif (selected_4=='Bar'):
+            print('ic50_bar.png')
+        elif (selected_4=='BeeSwarm'):
+            print('ic50_mw.png')
+        elif (selected_4=='HeatMap'):
+            print('ic50_heatmap')
+        elif (selected_4=='Important Features'):
+            print('ic50_importantfeatures')
+    elif (selected_2=='Model Performansı'):
+        selected_5 = st.multiselect('Lütfen model değerlendirme seçeneklerinden birini seçiniz', sec5)
+        if (selected_5=='RMSE'):
+            print('ic50_rmse.png')
+        elif (selected_5=='Time Taken'):
+            print('ic50_timetaken.png')
+    
     # Take Networkx graph and translate it to a PyVis graph format
     drug_net.from_nx(G)
 
@@ -53,31 +75,13 @@ else:
                         damping=0.95
                        )
 
-    # Save and read graph as HTML file (on Streamlit Sharing)
-    try:
-        path = '/tmp'
-        drug_net.save_graph(f'{path}/pyvis_graph.html')
-        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
-
-    # Save and read graph as HTML file (locally)
-    except:
-        path = '/html_files'
-        drug_net.save_graph(f'{path}/pyvis_graph.html')
-        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
-
-    # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+    
 
 # Footer
 st.markdown(
     """
     <br>
-    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-73qax4.streamlit.app/" target="_blank">Simple Example</a></h6>
-    <h6><a href="https://iuysal1905-streamlit-networkapp-kztp4y.streamlit.app/" target="_blank">Diabetes, Hypertension and Hyperlipidemia</a></h6>
-    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-serum-y63ywu.streamlit.app/" target="_blank">Interaction Type : Serum Concentration</a></h6>
-    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-adverse-yl8jbn.streamlit.app/" target="_blank">Interaction Type : Adverse </a></h6>
-    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-metabolism-9weo0f.streamlit.app/" target="_blank">Interaction Type : Metabolism </a></h6>
-    <h6><a href="https://iuysal1905-streamlit-pyvis-network-app-hypo-yrjex8.streamlit.app/" target="_blank">Interaction Type : Hypertensive and Hypotensive </a></h6>
+    <h6><a href="https://iuysal1905-streamlit-leukemia-dnhrmb.streamlit.app/" target="_blank">Lösemi İlaç-İlaç Etkileşim Ağ Grafiği</a></h6>
     <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
     """, unsafe_allow_html=True
     )
